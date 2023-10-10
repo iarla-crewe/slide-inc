816ceaf1081d08bc98d24db8d8b5a955dd6b8a91
Scott Geaney <Scott.Geaney@mycit.ie>
2023-10-10 13:06:04 +0100

    Add buttons and nav links between pages

--- mobile-app/app/src/main/java/com/slide/healthai/MainActivity.kt
+++ mobile-app/app/src/main/java/com/slide/healthai/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.slide.healthai
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    //private Button button; - scott
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Scott
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val Intent = Intent(this,MainActivity2::class.java)
+            startActivity(Intent)
+        }
+
+        val button1 = findViewById<Button>(R.id.button1)
+        button1.setOnClickListener {
+            val Intent = Intent(this,MainActivity3::class.java)
+            startActivity(Intent)
+        }
+
+        val button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener {
+            val Intent = Intent(this,MainActivity4::class.java)
+            startActivity(Intent)
+        }
+
     }
 }
+
+
