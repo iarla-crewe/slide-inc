909ca44dee99e1f86fdc5e5e3f9a358ada84356a
Leon Savva <Leon.savva1@gmail.com>
2023-10-22 12:22:06 +0100

    added activities and views for patient details

---
+++ mobile-app/app/src/main/java/com/slide/healthai/EntityDetails.kt
@@ -0,0 +1,23 @@
+package com.slide.healthai
+
+data class GpDetails(
+    val name: String,
+    val practiceName: String,
+    val mobileNumber: String
+)
+
+data class InsuranceCompanyDetails(
+    val name: String,
+    val phoneNumber: String,
+    val website: String
+)
+
+data class PatientDetails(
+    val name: String,
+    val age: Int,
+    val gender: String,
+    val height: Double,
+    val weight: Double,
+    val medicalHistory: String
+)
+
---
+++ mobile-app/app/src/main/java/com/slide/healthai/InsuranceDetailsActivity.kt
@@ -0,0 +1,4 @@
+package com.slide.healthai
+
+class InsuranceDetailsActivity {
+}
