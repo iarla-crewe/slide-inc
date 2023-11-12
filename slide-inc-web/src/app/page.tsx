156a356684b887c82f0c5273479c13cb0d7aa90d
Nelo0 <83479019+Nelo0@users.noreply.github.com>
2023-11-12 22:34:23 +0000

    Reverted to previous commit

--- slide-inc-web/src/app/page.tsx
+++ slide-inc-web/src/app/page.tsx
@@ -1,34 +1,34 @@
 import React from 'react';
 import Link from 'next/link';
-import { redirect } from 'next/navigation';
+import Header from './components/header';
 
-export default function Home() {
-  let isSignedIn = true;
-
-  if (isSignedIn) redirect("/patients");
-
+const Home: React.FC = () => {
   return (
+  
     <div>
-        <main className="flex flex-col items-center justify-center min-h-screen">
+      <Header/>
+      <main className="flex flex-col items-center justify-center min-h-screen">
         <h1 className="text-5xl font-bold mb-8">Welcome to HealthAI</h1>
         <p className="text-xl text-center mb-8">
-            Unlock the Power of AI in Healthcare
+          Unlock the Power of AI in Healthcare
         </p>
         <div className="flex flex-col space-y-4">
-            <Link href="./patientList">
+          <Link href="./patientList">
             <span className="google-login-button">
-                <span className="button-icon" />
-                Login with Google
+              <span className="button-icon" />
+              Login with Google
             </span>
-            </Link>
+          </Link>
         </div>
         <p className="text-lg mt-6">
-            New to HealthAI?{' '}
-            <Link href="./registration">
+          New to HealthAI?{' '}
+          <Link href="./registration">
             <span className="text-blue-500">Create an Account</span>
-            </Link>
+          </Link>
         </p>
-        </main>
+      </main>
     </div>
-);
+  );
 }
+
+export default Home;
