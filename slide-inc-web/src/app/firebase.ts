b2c1524ef41ca88153d26b092fddc6461c8f597f
Nelo0 <83479019+Nelo0@users.noreply.github.com>
2023-11-12 22:39:47 +0000

    Add getAuth and export declarations to firebase.ts

--- slide-inc-web/src/app/firebase.ts
+++ slide-inc-web/src/app/firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { getApps, getApp, initializeApp } from "firebase/app";
+import {  getAuth } from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,3 +19,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth()
+
+export {app, auth}
