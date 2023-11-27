e70fd47f0475b9811c1735a701ed2c77c6410565
Nelo0 <83479019+Nelo0@users.noreply.github.com>
2023-11-27 17:21:23 +0000

    Create edit patients dir and routes for editPatients

---
+++ slide-inc-web/src/app/(editPatients)/editMenu/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Patient from '../../components/patient';
+import styles from './Patients.module.css'
+
+export default function editMenu() {
+
+  return (
+    <div >
+      <h2>Patients Edit Menu</h2>
+    </div>
+  );
+}
