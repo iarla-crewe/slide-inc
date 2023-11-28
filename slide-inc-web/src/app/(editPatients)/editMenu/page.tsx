9854bc0407b38439c82cfcb410cfb3b755138a40
Nelo0 <83479019+Nelo0@users.noreply.github.com>
2023-11-28 00:45:11 +0000

    Add header to editPatients pages

--- slide-inc-web/src/app/(editPatients)/editMenu/page.tsx
+++ slide-inc-web/src/app/(editPatients)/editMenu/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Link from 'next/link'
+import Header from '@/app/components/header';
 
 export default function editMenu() {
 
   return (
     <div >
+      <Header/>
       <h2>Patients Edit Menu</h2>
 
       <Link href="/addPatient">
