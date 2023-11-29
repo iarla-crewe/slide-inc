e2cf083446eb7f0523f09c6d7d57bfd35f906084
Leon Savva R00220229 <leonids.savva@mycit.ie>
2023-11-29 17:39:19 +0000

    can read the logged in users data and display it

--- mobile-app/app/src/main/java/com/slide/healthai/UI/LoginActivity.kt
+++ mobile-app/app/src/main/java/com/slide/healthai/UI/LoginActivity.kt
@@ -7,7 +7,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.slide.healthai.MainActivity
 import com.slide.healthai.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -31,7 +30,7 @@ class LoginActivity : AppCompatActivity() {
                         // Sign in success
                         Log.d("LoginActivity", "signInWithEmail:success")
                         val user = auth.currentUser
-                        updateUI(user) // Implement this method to update UI and navigate
+                        updateUI(user)
                     } else {
                         // If sign in fails
                         Log.w("LoginActivity", "signInWithEmail:failure", task.exception)
@@ -43,18 +42,14 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun updateUI(user: FirebaseUser?) {
-        // Update your UI based on user object
         if (user != null) {
-            // Navigate to MainActivity or your home activity
-            val intent = Intent(this, MainActivity::class.java)
+            // User is signed in, navigate to PatientDetailsActivity
+            val intent = Intent(this, PatientDetailsActivity::class.java)
             startActivity(intent)
-            finish() // Finish this activity so the user can't navigate back to it
+            finish() // Finish this activity
         } else {
-            // Optionally, handle UI changes when sign-in fails
+            // User sign-in failed, handle the UI here
+            // For example, clear the input fields or show an error message
         }
-
     }
-
-
-
 }
