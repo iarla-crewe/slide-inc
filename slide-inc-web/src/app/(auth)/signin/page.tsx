3d59dc8c325619300e30ec99a72a9d6e4dbf299e
Iarla Crewe <44999892+iarla-crewe@users.noreply.github.com>
2023-12-21 15:37:07 +0000

    Add CSS to /signin and /signup

--- slide-inc-web/src/app/(auth)/signin/page.tsx
+++ slide-inc-web/src/app/(auth)/signin/page.tsx
@@ -1,7 +1,9 @@
 'use client';
-import { signIn, useSession } from 'next-auth/react';
-import { useRouter, redirect } from 'next/navigation';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+
+import style from '../auth.module.css'
 
 export default function Signin() {
   const [email, setEmail] = useState('');
@@ -10,17 +12,20 @@ export default function Signin() {
 
   return (
     <>
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-gray-100">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-black">
+    <div className={style.mainContainer}>
+        <div className={style.headerContainer}>
+          <h1 className={style.title}>
+            HealthAI
+          </h1>
+          <h2 className={style.signInHeading}>
             Sign in to your account
           </h2>
         </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className={style.formContainer}>
           <div className="space-y-6">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-black">
+              <label htmlFor="email" className={style.formLabel}>
                 Email address
               </label>
               <div className="mt-2">
@@ -31,18 +36,18 @@ export default function Signin() {
                   autoComplete="email"
                   onChange={(e) => setEmail(e.target.value)}
                   required
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-black shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  className={style.formField}
                 />
               </div>
             </div>
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-black">
+                <label htmlFor="password" className={style.formLabel}>
                   Password
                 </label>
                 <div className="text-sm">
-                  <div onClick={() => router.push('/forgot-password')} className="cursor-pointer font-semibold text-indigo-400 hover:text-indigo-300">
+                  <div onClick={() => router.push('/forgot-password')} className={style.heavyLink}>
                     Forgot password?
                   </div>
                 </div>
@@ -55,7 +60,7 @@ export default function Signin() {
                   autoComplete="current-password"
                   onChange={(e) => setPassword(e.target.value)}
                   required
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-black shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  className={style.formField}
                 />
               </div>
             </div>
@@ -64,7 +69,7 @@ export default function Signin() {
               <button
                 onClick={() => signIn('credentials', {email, password, redirect: true, callbackUrl: '/patients'})}
                 disabled={!email || !password}
-                className="disabled:opacity-40 flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                className={style.button}
               >
                 Sign in
               </button>
@@ -73,7 +78,7 @@ export default function Signin() {
 
           <p className="mt-10 text-center text-sm text-gray-400">
             Not a member?{' '}
-            <button onClick={() => router.push('signup')} className="font-semibold leading-6 text-indigo-400 hover:text-indigo-300">
+            <button onClick={() => router.push('signup')} className={style.link}>
               Sign Up
             </button>
           </p>
