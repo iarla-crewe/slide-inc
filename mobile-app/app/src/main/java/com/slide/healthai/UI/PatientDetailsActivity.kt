5d046aa81f78fde1c7f108d45ab51accef86433b
Leon Savva R00220229 <leonids.savva@mycit.ie>
2023-12-21 16:29:06 +0000

    Contacting parties

--- mobile-app/app/src/main/java/com/slide/healthai/UI/PatientDetailsActivity.kt
+++ mobile-app/app/src/main/java/com/slide/healthai/UI/PatientDetailsActivity.kt
@@ -1,8 +1,8 @@
 package com.slide.healthai.UI
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -21,6 +21,7 @@ class PatientDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPatientDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupContactButtons()
 
         binding.btnNavigateToChat.setOnClickListener {
             val intent = Intent(this, ChatActivity::class.java)
@@ -37,6 +38,25 @@ class PatientDetailsActivity : AppCompatActivity() {
 
         fetchPatientDetails()
     }
+
+    private fun setupContactButtons() {
+        binding.contactButton1.setOnClickListener {
+            openWebPage("https://www1.vhi.ie/help-and-support/contact") // Replace with your first URL
+        }
+
+        binding.contactButton2.setOnClickListener {
+            openWebPage("https://www.corkcitymedicalcentre.com/") // Replace with your second URL
+        }
+    }
+
+    private fun openWebPage(url: String) {
+        val webPage: Uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, webPage)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
 
     private fun fetchPatientDetails() {
         val currentUser = auth.currentUser
@@ -57,9 +77,9 @@ class PatientDetailsActivity : AppCompatActivity() {
                                 // Update UI with patient details
                                 binding.tvUserName.text = name
                                 binding.tvUserEmail.text = email
-                                binding.tvUserHeight.text = "Height: $height"
-                                binding.tvUserWeight.text = "Weight: $weight"
-                                binding.tvUserGender.text = "Gender: $gender"
+                                binding.tvUserHeight.text = height
+                                binding.tvUserWeight.text = weight
+                                binding.tvUserGender.text = gender
                             }
                         } else {
                             showNoData()
@@ -89,105 +109,4 @@ class PatientDetailsActivity : AppCompatActivity() {
         finish()
     }
 
-    // ChatGPT integration
-
-    private var stringOutput: String = ""
-    private lateinit var textView: TextView
-    val stringAPIKey = "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"
-    val stringURLEndPoint = "https://api.openai.com/v1/chat/completions"
-
-//    fun buttonChatGPT(view: View) {
-//        val userMessage = binding.etChatInput.text.toString()
-//
-//        if (userMessage.isBlank()) {
-//            Toast.makeText(this, "Please enter a message.", Toast.LENGTH_SHORT).show()
-//            return
-//        }
-//
-//        val jsonObject = JSONObject()
-//        try {
-//            jsonObject.put("model", "gpt-3.5-turbo")
-//
-//            val jsonArrayMessage = JSONArray()
-//            val jsonObjectMessage = JSONObject().apply {
-//                put("role", "user")
-//                put("content", userMessage)  // User's input as content
-//            }
-//            jsonArrayMessage.put(jsonObjectMessage)
-//
-//            jsonObject.put("messages", jsonArrayMessage)
-//        } catch (e: JSONException) {
-//            throw RuntimeException(e)
-//        }
-//
-//        val jsonObjectRequest = object : JsonObjectRequest(
-//            Request.Method.POST, stringURLEndPoint, jsonObject,
-//            Response.Listener { response ->
-//                if (response != null) {
-//                    // Response received from the API
-//                    val stringText: String? = try {
-//                        response.getJSONArray("choices")
-//                            .getJSONObject(0)
-//                            .getJSONObject("message")
-//                            .getString("content")
-//                    } catch (e: JSONException) {
-//                        null // Handle JSON parsing error
-//                    }
-//
-//                    if (stringText != null) {
-//                        // Display the received text
-//                        stringOutput += stringText
-//                        textView.text = stringOutput
-//                    } else {
-//                        // Handle the case where the response doesn't contain the expected data
-//                        Log.e("API Response", "Unexpected or missing data in the API response")
-//                    }
-//                } else {
-//                    // Handle the case where no response is received
-//                    Log.e("API Response", "No response received from the API")
-//                }
-//            },
-//            Response.ErrorListener { error ->
-//                if (error is VolleyError) {
-//                    if (error.networkResponse != null) {
-//                        val statusCode = error.networkResponse.statusCode
-//                        val errorMessage = String(error.networkResponse.data)
-//                        Log.e(
-//                            "Volley Error",
-//                            "Status Code: $statusCode, Error Message: $errorMessage"
-//                        )
-//
-//                        // You can display a message to the user or take other actions based on the error.
-//                    } else {
-//                        Log.e("Volley Error", "Network response is null")
-//                        // Handle other types of Volley errors
-//                    }
-//                } else {
-//                    Log.e("Volley Error", "Non-Volley error: ${error.localizedMessage}")
-//                    // Handle non-Volley errors
-//                }
-//            }
-//        ) {
-//            @Throws(AuthFailureError::class)
-//            override fun getHeaders(): Map<String, String> {
-//                val mapHeader = HashMap<String, String>()
-//                mapHeader["Authorization"] = "Bearer $stringAPIKey"
-//                mapHeader["Content-Type"] = "application/json"
-//                return mapHeader
-//            }
-//
-//            override fun parseNetworkResponse(response: NetworkResponse): Response<JSONObject> {
-//                return super.parseNetworkResponse(response)
-//            }
-//        }
-//
-//        val intTimeoutPeriod = 60000 // 60 seconds timeout duration defined
-//        val retryPolicy = DefaultRetryPolicy(
-//            intTimeoutPeriod,
-//            DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-//            DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
-//        )
-//        jsonObjectRequest.retryPolicy = retryPolicy
-//        Volley.newRequestQueue(applicationContext).add(jsonObjectRequest)
-//    }
 }
