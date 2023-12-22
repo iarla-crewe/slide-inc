929263c0fa2225e6700b5c5d786574ac3104d981
Iarla Crewe <44999892+iarla-crewe@users.noreply.github.com>
2023-12-22 18:23:26 +0000

    Add patient not found screen

--- slide-inc-web/src/app/patients/[phone]/page.tsx
+++ slide-inc-web/src/app/patients/[phone]/page.tsx
@@ -9,9 +9,21 @@ import style from '../patients.module.css'
 export default async function PatientDetails({ params }: { params: { phone: string }}) {
     const formattedPhone = params.phone.replace("%2B", "+").replace("%2", "+")
     const patient = await getPatient(formattedPhone);
-    if (patient == null) return (<div/>)
 
-    // {patient.name}
+    if (patient == null) return (
+        <div className={style.body}>
+            <Header/>
+            <div className={style.patientDetails}>
+                <div className={style.container}>
+                    <div className={style.patientName}>
+                        <h1 className={style.h1}>Could not find patient with phone number: {formattedPhone}</h1>
+                        <a href="/patients" className={style.subheaderLink}>Go Back</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
     // {displayGender(patient.sex)}
     // {patient.dob}
 
@@ -21,7 +33,7 @@ export default async function PatientDetails({ params }: { params: { phone: stri
             <div className={style.patientDetails}>
                 <div className={style.container}>
                     <div className={style.patientName}>
-                        <span className={style.subheader}>Patient</span>
+                        <p className={style.subheader}>Patient</p>
                         <h1 className={style.h1}>{patient.name}</h1>
                     </div>
                 </div>
